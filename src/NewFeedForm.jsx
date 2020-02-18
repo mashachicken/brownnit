7bb5774e0fe2ec2aab33aa1e2de91bf5b8bf0738
Student <example@example.com>
2020-02-18 10:20:34 -0800

    add code to empty fields after sumbmission

--- src/NewFeedForm.jsx
+++ src/NewFeedForm.jsx
@@ -6,31 +6,34 @@ function NewFeedForm(){
   let _issue = null;
 
   function handleNewFormSubmission(event) {
-  event.preventDefault();
-  console.log(_names.value);
-  console.log(_location.value);
-  console.log(_issue.value);
-}
+    event.preventDefault();
+    console.log(_names.value);
+    console.log(_location.value);
+    console.log(_issue.value);
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
 
-return (
-  <div>
+  return (
+    <div>
     <h1> New Feed </h1>
     <form onSubmit={handleNewFormSubmission}>
-        <input
-          type='text'
-          id='names'
-          ref={(input) => {_names = input;}}/>
-        <input
-          type='text'
-          id='location'
-          ref={(input) => {_location = input;}}/>
-        <textarea
-          id='post'
-          ref={(textarea) => {_issue = textarea;}}/>
-        <button type='submit'>Post!</button>
-      </form>
-  </div>
-);
+    <input
+    type='text'
+    id='names'
+    ref={(input) => {_names = input;}}/>
+    <input
+    type='text'
+    id='location'
+    ref={(input) => {_location = input;}}/>
+    <textarea
+    id='post'
+    ref={(textarea) => {_issue = textarea;}}/>
+    <button type='submit'>Post!</button>
+    </form>
+    </div>
+  );
 }
 
 export default NewFeedForm;
